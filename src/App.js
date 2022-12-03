716c5481070506487b1a23ed7ab4c2847116ac00
Nhat Bao <nhatbaoxxd@gmail.com>
2022-12-03 20:29:18 +0700

    react basic
    - create react app
    - JSX
    - component
    - props
    - css
    - rendering
    - refactor code
    - props children

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import YoutubeList from './components/Youtube/YoutubeList'
 
 function App() {
+  const name = 'Evondev'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <YoutubeList>
+        {/* children: html, component, text, variable */}
+        {name}
+      </YoutubeList>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/videoData.js
@@ -0,0 +1,41 @@
+export const videoData = [
+  {
+    id: 1,
+    thumbnail: 'https://picsum.photos/id/27/367/267',
+    avatar: 'https://picsum.photos/id/64/367/267',
+    title: 'Learn React with Evondev',
+    channel: 'Evondev',
+    view: '10N',
+    time: '2 ngày',
+  },
+
+  {
+    id: 2,
+    thumbnail: 'https://picsum.photos/id/28/367/267',
+    avatar: 'https://picsum.photos/id/65/367/267',
+    title: 'Learn React with Easy Frontend',
+    channel: 'Easy Frontend',
+    view: '15N',
+    time: '3 ngày',
+  },
+
+  {
+    id: 3,
+    thumbnail: 'https://picsum.photos/id/29/367/267',
+    avatar: '',
+    title: 'Learn React with Hoi Dan IT',
+    channel: 'Hoi Dan IT',
+    view: '5N',
+    time: '1 ngày',
+  },
+
+  {
+    id: 4,
+    thumbnail: 'https://picsum.photos/id/182/367/267',
+    avatar: 'https://picsum.photos/id/177/367/267',
+    title: 'Learn React with Du Thanh Duoc',
+    channel: 'Duoc Dev',
+    view: '6N',
+    time: '2 ngày',
+  },
+]
